dd3ced43b2a8f828fc9393a2ce3f01eb60ced411
colt <necromanga@gmail.com>
2016-11-29 18:44:11 +0100

    Initial commit.

---
+++ app/src/main/java/com/koltinjo/android162_calculator/ActivityMain.java
@@ -0,0 +1,15 @@
+package com.koltinjo.android162_calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// https://youtu.be/pjTAF53qRA4?list=PLEVlop6sMHCp3Wp0mqT2-OxHwVdAod1uy
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
