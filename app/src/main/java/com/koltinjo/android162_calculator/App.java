a02d2a470928195186ac590d9a4f23abf5da25c8
colt <necromanga@gmail.com>
2016-12-01 13:08:22 +0100

    Added Calculation class.

---
+++ app/src/main/java/com/koltinjo/android162_calculator/App.java
@@ -0,0 +1,25 @@
+package com.koltinjo.android162_calculator;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by colt on 01.12.2016.
+ */
+
+// TODO Check for memory leaks.
+public class App extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this;
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+
+}
---
+++ app/src/main/java/com/koltinjo/android162_calculator/Calculation.java
@@ -0,0 +1,112 @@
+package com.koltinjo.android162_calculator;
+
+import org.javia.arity.Symbols;
+import org.javia.arity.SyntaxException;
+
+/**
+ * Created by colt on 30.11.2016.
+ */
+
+// Model.
+public class Calculation {
+
+    private final Symbols symbols;
+    private static String currentExpression;
+    private CalculationResult calculationResult;
+
+    // Constructor.
+    public Calculation() {
+        symbols = new Symbols();
+    }
+
+    // Delete a single character from currentExpression, unless empty.
+    public void deleteCharacter() {
+        if (currentExpression.length() > 0) {
+            currentExpression = currentExpression.substring(0, currentExpression.length() - 1);
+            calculationResult.onExpressionChange(currentExpression, true);
+        } else {
+            calculationResult.onExpressionChange(App.getContext().getString(R.string.input_invalid), false);
+        }
+    }
+
+    // Delete entire expression, unless empty.
+    public void deleteExpression() {
+        if (currentExpression.isEmpty()) {
+            calculationResult.onExpressionChange(App.getContext().getString(R.string.input_invalid), false);
+        }
+        currentExpression = "";
+        calculationResult.onExpressionChange(currentExpression, true);
+    }
+
+    // Append number to currentExpression if valid.
+    public void appendNumber(String number) {
+        if (currentExpression.startsWith("0") && number.equals("0")) {
+            calculationResult.onExpressionChange(App.getContext().getString(R.string.input_invalid), false);
+        } else {
+            if (currentExpression.length() > 16) {
+                calculationResult.onExpressionChange(App.getContext().getString(R.string.expression_long), false);
+            } else {
+                // TODO currentExpression += number;
+                calculationResult.onExpressionChange(currentExpression, true);
+            }
+        }
+    }
+
+    // Append operator "+-*/" to currentExpression.
+    public void appendOperator(String operator) {
+        if (validateExpression()) {
+            currentExpression += operator;
+            calculationResult.onExpressionChange(currentExpression, true);
+        }
+    }
+
+    // Append decimal sign to currentExpression.
+    public void appendDecimal() {
+        if (validateExpression()) {
+            currentExpression += ".";
+            calculationResult.onExpressionChange(currentExpression, true);
+        }
+    }
+
+    // If currentExpression passes checks, pass currentExpression to symbols object, and return the result.
+    public void performEvaluate() {
+        if (validateExpression()) {
+            try {
+                Double result = symbols.eval(currentExpression);
+                currentExpression = Double.toString(result);
+                calculationResult.onExpressionChange(currentExpression, true);
+            } catch (SyntaxException e) {
+                calculationResult.onExpressionChange(App.getContext().getString(R.string.input_invalid), false);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Helper method to validate expression.
+    private boolean validateExpression() {
+        if (currentExpression.endsWith("+") || currentExpression.endsWith("-") || currentExpression.endsWith("*") || currentExpression.endsWith("/")) {
+            calculationResult.onExpressionChange(App.getContext().getString(R.string.input_invalid), false);
+            return false;
+        } else if (currentExpression.isEmpty()) {
+            calculationResult.onExpressionChange(App.getContext().getString(R.string.expression_empty), false);
+            return false;
+        } else if (currentExpression.length() > 16) {
+            calculationResult.onExpressionChange(App.getContext().getString(R.string.expression_long), false);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    // Setter.
+    public void setCalculationResultListener(CalculationResult calculationResult) {
+        this.calculationResult = calculationResult;
+        currentExpression = "";
+    }
+
+    interface CalculationResult{
+        void onExpressionChange(String result, boolean successful);
+    }
+
+
+}
