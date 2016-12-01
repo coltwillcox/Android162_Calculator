05459fa08938c2f8f4649d749a7d8a36ec2bc7b0
colt <necromanga@gmail.com>
2016-12-01 15:09:33 +0100

    Changed app name. Fixed layouts.

--- app/src/main/java/com/koltinjo/android162_calculator/ActivityMain.java
+++ app/src/main/java/com/koltinjo/android162_calculator/ActivityMain.java
@@ -3,6 +3,17 @@ package com.koltinjo.android162_calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// TODO App name.
+// TODO Icon.
+// TODO Options.
+// TODO Options: Commas.
+// TODO Options: Vibrations.
+// TODO Options: Sounds.
+// TODO Options: Decimal size.
+// TODO Save currentExpression on config change.
+// TODO Multiple "=" presses.
+// TODO Sciemtific look? Options?
+
 // https://youtu.be/pjTAF53qRA4?list=PLEVlop6sMHCp3Wp0mqT2-OxHwVdAod1uy
 public class ActivityMain extends AppCompatActivity {
 
